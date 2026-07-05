375d9dc1588a027bfc6a4032ad9ba0a407ad22c2
technikerleben <christophvillis@googlemail.com>
2026-07-05 09:40:15 +0200

    Mount integrated 3D overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThreeDOverlay } from './components/ThreeDOverlay';
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById('root')!).render(<StrictMode><App /><ThreeDOverlay /></StrictMode>);
